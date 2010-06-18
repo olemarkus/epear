ee01c97fe0908050aa14fcb5f4c13c2b593d061b
Ole Markus With <olemarkus@olemarkus.org>
2010-06-18 21:53:17 +0200

    Cleaned up syntax a little

--- epear.php
+++ epear.php
@@ -1,11 +1,21 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/** 
+ * This file contains the epear program
+ *
+ * PHP Version 5
+ *
+ * @author    Ole Markus With <olemarkus@olemarkus.org>
+ * @copyright 2010 Ole Markus With
+ */
 
 require_once "PEAR/Config.php";
 require_once "PEAR/PackageFile.php";
 
 $package = $argv[1];
 
-$config = PEAR_Config::singleton('','');
+$config = PEAR_Config::singleton('', '');
 
 $packageFile = new PEAR_PackageFile($config);
 
@@ -36,13 +46,13 @@ $version = $url["version"];
 
 
 if (!is_dir('distfiles')) {
-	mkdir('distfiles');
+    mkdir('distfiles');
 }
 
 $filename = 'distfiles/' . $name . "-" . $version . ".tgz";
 
 if (!file_exists($filename)) {
-	passthru("wget $uri -O $filename");
+    passthru("wget $uri -O $filename");
 }
 
 $pf = $packageFile->fromAnyFile($filename, PEAR_VALIDATE_NORMAL);
@@ -61,31 +71,32 @@ $pearDeps = array();
 $usedep["dom"] = "xml";
 $usedep["mbstring"] = "unicode";
 
-foreach($pf->getDeps() as $dep) {
-	if ($dep["optional"] == "yes") continue;
+foreach ($pf->getDeps() as $dep) {
+    if ($dep["optional"] == "yes") continue;
 
-	switch ($dep["type"]) {
-	case "pkg": 
-		$prefix = "";
-		if ($dep["channel"] == "pear.php.net") $prefix = "PEAR-";
-		$pearDeps[] = "dev-php/" . $prefix . $dep["name"];
-		break;
-	case "ext":
-		if (isset($usedep[$dep["name"]])) $dep["name"] = $usedep[$dep["name"]];
-		if (!in_array($dep["name"], array("pcre","spl")))
-			$php53flags[] = $dep["name"];
-		$phpflags[] = $dep["name"];
-	}
+    switch ($dep["type"]) {
+    case "pkg": 
+        $prefix = "";
+        if ($dep["channel"] == "pear.php.net") $prefix = "PEAR-";
+        $pearDeps[] = "dev-php/" . $prefix . $dep["name"];
+        break;
+    case "ext":
+        if (isset($usedep[$dep["name"]])) $dep["name"] = $usedep[$dep["name"]];
+        if (!in_array($dep["name"], array("pcre","spl")))
+            $php53flags[] = $dep["name"];
+        $phpflags[] = $dep["name"];
+    }
 }
 
 
 
 if ($phpflags != $php53flags) {
-	$phpdep = "|| ( <dev-lang/php-5.3[" . implode(",", $phpflags) . "] >=dev-lang/php-5.3[" . implode(",", $php53flags) . "] )" ;
+    $phpdep = "|| ( <dev-lang/php-5.3[" . implode(",", $phpflags) . "] " .
+        ">=dev-lang/php-5.3[" . implode(",", $php53flags) . "] )" ;
 } elseif ($phpflags) {
-	$phpdep = "dev-lang/php[" . implode(",", $phpflags). "]";
+    $phpdep = "dev-lang/php[" . implode(",", $phpflags). "]";
 } else {
-	$phpdep = "dev-lang/php";
+    $phpdep = "dev-lang/php";
 }
 
 $peardep = implode("\n", $pearDeps);
@@ -93,17 +104,18 @@ $peardep = implode("\n", $pearDeps);
 $doins = "";
 
 foreach ($filelist as $filename => $file) {
-	
+
 }
 
 
 $prefix = ($channelUri == "pear.php.net") ? $prefix = "PEAR-" : "";
 
-if(!is_dir("overlay/dev-php/" . $prefix . $pf->getName())) {
-	mkdir("overlay/dev-php/" . $prefix . $pf->getName(), 0777, true);
+if (!is_dir("overlay/dev-php/" . $prefix . $pf->getName())) {
+    mkdir("overlay/dev-php/" . $prefix . $pf->getName(), 0777, true);
 }
 
-$ebuildname = "overlay/dev-php/" . $prefix . $pf->getName() . "/" . $prefix . $pf->getName() . "-" . $pf->getVersion() . ".ebuild";
+$ebuildname = "overlay/dev-php/" . $prefix . $pf->getName() . "/" . 
+    $prefix . $pf->getName() . "-" . $pf->getVersion() . ".ebuild";
 
 $ebuild = `head -n4 /usr/portage/skel.ebuild`;
 
