2d7a773b5f7c6c845e79a6ff9508e5ecb32bb01f
Ole Markus With <olemarkus@olemarkus.org>
2010-06-26 18:40:51 +0200

    Now also generates all missing dependencies

--- epear.php
+++ epear.php
@@ -10,6 +10,8 @@
  * @copyright 2010 Ole Markus With
  */
 
+require_once "PEAR/Config.php";
+require_once "PEAR/PackageFile.php";
 
 function cleanup_version($version) 
 {
@@ -35,166 +37,185 @@ function get_channel_prefix($channelUri)
     return $prefix;
 }
 
-require_once "PEAR/Config.php";
-require_once "PEAR/PackageFile.php";
+function generate_ebuild($pear_package) {
+    echo "Generating ebuild for $pear_package\n";
+    $config = PEAR_Config::singleton('', '');
 
-$package = $argv[1];
+    $packageFile = new PEAR_PackageFile($config);
 
-$config = PEAR_Config::singleton('', '');
+    $channelName =  $config->get('default_channel');
 
-$packageFile = new PEAR_PackageFile($config);
+    $parsedName = $config->getRegistry()->parsePackageName($pear_package,
+        $channelName);
 
-$channelName =  $config->get('default_channel');
+    $channelUri = $parsedName["channel"];
 
-$parsedName = $config->getRegistry()->parsePackageName($package, $channelName);
+    $channel = $config->getRegistry()->getChannel($parsedName['channel']);
 
-$channelUri = $parsedName["channel"];
+    $base = $channel->getBaseURL('REST1.3', $parsedName['channel']);
+    $restv = "1.3";
 
-$channel = $config->getRegistry()->getChannel($parsedName['channel']);
-
-$base = $channel->getBaseURL('REST1.3', $parsedName['channel']);
-$restv = "1.3";
-
-if (!$base) {
-    $base = $channel->getBaseURL('REST1.0', $parsedName['channel']);
-    $restv = "1.0";
-}
-
-$options = array();
-
-$rest = $config->getRest($restv, $options);
-
-$state = 'alpha';
-
-
-$url = $rest->getDownloadUrl($base, $parsedName, $state, false, $parsedName['channel']);
-
-if (PEAR::isError($url)) {
-    die("Failed to obtain url for $channelUri\n");
-
-}
-
-$uri = $url["url"] . ".tgz";
-
-$name = $url["package"];
-$version = $url["version"];
-
-
-if (!is_dir('distfiles')) {
-    mkdir('distfiles');
-}
-
-$filename = 'distfiles/' . $name . "-" . $version . ".tgz";
-
-if (!file_exists($filename)) {
-    passthru("wget $uri -O $filename");
-}
-
-$pf = $packageFile->fromAnyFile($filename, PEAR_VALIDATE_NORMAL);
-
-
-$filelist = $pf->getInstallationFileList();
-$fullfilelist = $pf->getFileList();
-
-$rmfiles = array_diff_key($fullfilelist, $filelist);
-
-
-$phpflags = array();
-$php53flags = array();
-$pearDeps = array();
-$postDeps = array();
-
-$usedep["dom"] = "xml";
-$usedep["mbstring"] = "unicode";
-
-foreach ($pf->getDeps() as $dep) {
-    if ($dep["optional"] == "yes") continue;
-
-    switch ($dep["type"]) {
-    case "pkg": 
-        $prefix = get_channel_prefix($dep["channel"]);
-        $rel = "";
-        if ($dep["rel"] == "ge") {
-            $rel = ">=";
-        }
-
-        //Certain packages tend to create circular deps. We hack them into PDEPEND
-        if ($dep["name"] == "PHPUnit") {
-            $postDeps[$dep["name"]] = $rel . get_package_name($prefix . $dep["name"]) . "-" . cleanup_version($dep["version"]);
-        } else {
-            //The key is used to prevent duplicates
-            $pearDeps[$dep["name"]] = $rel . get_package_name($prefix . $dep["name"]) . "-" . cleanup_version($dep["version"]);
-        }
-        break;
-    case "ext":
-        if (isset($usedep[$dep["name"]])) $dep["name"] = $usedep[$dep["name"]];
-        if (!in_array($dep["name"], array("pcre","spl","reflection")))
-            $php53flags[] = $dep["name"];
-        $phpflags[] = $dep["name"];
-        break;
-    case "php":
-        $phpver = $dep["version"];
+    if (!$base) {
+        $base = $channel->getBaseURL('REST1.0', $parsedName['channel']);
+        $restv = "1.0";
     }
+
+    $options = array();
+
+    $rest = $config->getRest($restv, $options);
+
+    $state = 'alpha';
+
+
+    $url = $rest->getDownloadUrl($base, $parsedName, $state, false,
+        $parsedName['channel']);
+
+    if (PEAR::isError($url)) {
+        die("Failed to obtain url for $channelUri\n");
+
+    }
+
+    $uri = $url["url"] . ".tgz";
+
+    $name = $url["package"];
+    $version = $url["version"];
+
+
+    if (!is_dir('distfiles')) {
+        mkdir('distfiles');
+    }
+
+    $filename = 'distfiles/' . $name . "-" . $version . ".tgz";
+
+    if (!file_exists($filename)) {
+        passthru("wget $uri -O $filename");
+    }
+
+    $pf = $packageFile->fromAnyFile($filename, PEAR_VALIDATE_NORMAL);
+
+
+    $filelist = $pf->getInstallationFileList();
+    $fullfilelist = $pf->getFileList();
+
+    $rmfiles = array_diff_key($fullfilelist, $filelist);
+
+
+    $phpflags = array();
+    $php53flags = array();
+    $pearDeps = array();
+    $postDeps = array();
+
+    $usedep["dom"] = "xml";
+    $usedep["mbstring"] = "unicode";
+
+    foreach ($pf->getDeps() as $dep) {
+        if ($dep["optional"] == "yes") continue;
+
+        switch ($dep["type"]) {
+        case "pkg":
+            
+            
+            $prefix = get_channel_prefix($dep["channel"]);
+            $rel = "";
+            if ($dep["rel"] == "ge") {
+                $rel = ">=";
+            }
+            
+            $pkgname = $rel . get_package_name($prefix . $dep["name"]) . "-" .
+                cleanup_version($dep["version"]);
+
+            //Certain packages tend to create circular deps. We hack them into
+            //PDEPEND
+            if ($dep["name"] == "PHPUnit") {
+                $postDeps[$dep["name"]] = $pkgname;
+            } else {
+                //The key is used to prevent duplicates
+                $pearDeps[$dep["name"]] = $pkgname;
+                if (!(shell_exec("portageq match / " . escapeshellarg($pkgname)))) {
+                echo "Dependency $pkgname not found\n";
+                    generate_ebuild($dep["channel"] . "/" . $dep["name"]);
+                }
+            }
+            
+            
+            
+            break;
+        case "ext":
+            if (isset($usedep[$dep["name"]])) $dep["name"] = $usedep[$dep["name"]];
+            if (!in_array($dep["name"], array("pcre","spl","reflection")))
+                $php53flags[] = $dep["name"];
+            $phpflags[] = $dep["name"];
+            break;
+        case "php":
+            $phpver = $dep["version"];
+        }
+    }
+
+    $phpdep = "";
+
+    if ($phpflags != $php53flags) {
+        $phpdep = "|| ( <dev-lang/php-5.3[" . implode(",", $phpflags) . "] " .
+            "    >=dev-lang/php-5.3[" . implode(",", $php53flags) . "] )\n    " ;
+    } elseif ($phpflags) {
+        $phpdep = "dev-lang/php[" . implode(",", $phpflags). "]\n    ";
+    }
+
+    $phpdep .= ">=dev-lang/php-$phpver";
+
+
+    $peardep = implode("\n    ", $pearDeps);
+
+    $postdep = implode("\n    ", $postDeps);
+
+    $doins = "";
+
+    $prefix = get_channel_prefix($channelUri); 
+
+
+
+    $ename = get_channel_prefix($channelUri) . $pf->getName();
+    $myp = $pf->getName();
+
+    $euri = str_replace($ename, $myp, $uri);
+
+    if (!is_dir("overlay/" . get_package_name($ename))) {
+        mkdir("overlay/" . get_package_name($ename), 0777, true);
+    }
+
+    $ebuildname = "overlay/" . get_package_name($ename)  . "/" . 
+        get_package_name($ename, false) . "-" . cleanup_version($pf->getVersion()) .
+".ebuild";
+
+    $ebuild = `head -n4 /usr/portage/skel.ebuild`;
+
+    $ebuild .= "EAPI=\"2\"\n";
+    $ebuild .= "\n";
+    $ebuild .= "PEAR_PV=\"" . $pf->getVersion() . "\"\n";
+    $ebuild .= "PHP_PEAR_PKG_NAME=\"" . $pf->getName() . "\"\n";
+    $ebuild .= "\n";
+    $ebuild .= "inherit php-pear-r1\n";
+    $ebuild .= "\n";
+    $ebuild .= "KEYWORDS=\"~" . `portageq envvar ARCH` . "\"\n";
+    $ebuild .= "SLOT=\"0\"\n";
+    $ebuild .= "DESCRIPTION=\"" . $pf->getSummary() . "\"\n";
+    $ebuild .= "LICENSE=\"" . str_replace(" License", "", $pf->getLicense()) .
+"\"\n";
+    $ebuild .= "HOMEPAGE=\"" . $parsedName['channel'] . "\"\n";
+    $ebuild .= "SRC_URI=\"" . $euri . "\"\n";
+    $ebuild .= "\n";
+    $ebuild .= "DEPEND=\"" . $phpdep . "\n    " . $peardep . "\"\n";
+    $ebuild .= "RDEPEND=\"\${DEPEND}\"\n";
+    if ($postdep) {
+        $ebuild .= "PDEPEND=\"$postdep\"\n";
+    }
+    $ebuild .= "\n";
+
+    file_put_contents($ebuildname, $ebuild);
+
+    echo "Ebuild written to $ebuildname\n";
+    
+    passthru("ebuild $ebuildname manifest");
 }
 
-$phpdep = "";
 
-if ($phpflags != $php53flags) {
-    $phpdep = "|| ( <dev-lang/php-5.3[" . implode(",", $phpflags) . "] " .
-        "    >=dev-lang/php-5.3[" . implode(",", $php53flags) . "] )\n    " ;
-} elseif ($phpflags) {
-    $phpdep = "dev-lang/php[" . implode(",", $phpflags). "]\n    ";
-}
-
-$phpdep .= ">=dev-lang/php-$phpver";
-
-
-$peardep = implode("\n    ", $pearDeps);
-
-$postdep = implode("\n    ", $postDeps);
-
-$doins = "";
-
-$prefix = get_channel_prefix($channelUri); 
-
-
-
-$ename = get_channel_prefix($channelUri) . $pf->getName();
-$myp = $pf->getName();
-
-$euri = str_replace($ename, $myp, $uri);
-
-if (!is_dir("overlay/" . get_package_name($ename))) {
-    mkdir("overlay/" . get_package_name($ename), 0777, true);
-}
-
-$ebuildname = "overlay/" . get_package_name($ename)  . "/" . 
-    get_package_name($ename, false) . "-" . cleanup_version($pf->getVersion()) . ".ebuild";
-
-$ebuild = `head -n4 /usr/portage/skel.ebuild`;
-
-$ebuild .= "EAPI=\"2\"\n";
-$ebuild .= "\n";
-$ebuild .= "PEAR_PV=\"" . $pf->getVersion() . "\"\n";
-$ebuild .= "PHP_PEAR_PKG_NAME=\"" . $pf->getName() . "\"\n";
-$ebuild .= "\n";
-$ebuild .= "inherit php-pear-r1\n";
-$ebuild .= "\n";
-$ebuild .= "KEYWORDS=\"~amd64\"\n";
-$ebuild .= "SLOT=\"0\"\n";
-$ebuild .= "DESCRIPTION=\"" . $pf->getSummary() . "\"\n";
-$ebuild .= "LICENSE=\"" . str_replace(" License", "", $pf->getLicense()) . "\"\n";
-$ebuild .= "HOMEPAGE=\"" . $parsedName['channel'] . "\"\n";
-$ebuild .= "SRC_URI=\"" . $euri . "\"\n";
-$ebuild .= "\n";
-$ebuild .= "DEPEND=\"" . $phpdep . "\n    " . $peardep . "\"\n";
-$ebuild .= "RDEPEND=\"\${DEPEND}\"\n";
-if ($postdep) {
-    $ebuild .= "PDEPEND=\"$postdep\"\n";
-}
-$ebuild .= "\n";
-
-file_put_contents($ebuildname, $ebuild);
-
-echo "Ebuild written to $ebuildname\n";
-passthru("ebuild $ebuildname manifest");
+generate_ebuild($package = $argv[1]);
