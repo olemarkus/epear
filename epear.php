d84523b4cbb490b6c0e310c4433154555792ec9a
Denny Reeh <denny.reeh@gmail.com>
2011-08-29 12:27:30 +0200

    fixed pear dep without versionnumber

--- epear.php
+++ epear.php
@@ -128,9 +128,11 @@ function generate_ebuild($pear_package)
             if ($dep["rel"] == "ge") {
                 $rel = ">=";
             }
-            
-            $pkgname = $rel . get_package_name($prefix . $dep["name"]) . "-" .
-                cleanup_version($dep["version"]);
+
+            $pkgname = $rel . get_package_name($prefix . $dep["name"]);
+            if (cleanup_version($dep["version"])) {
+                $pkgname .= "-" . cleanup_version($dep["version"]);
+            }
 
             //Certain packages tend to create circular deps. We hack them into
             //PDEPEND
